0fdc3c98744ab48642386696f7dd0d26f89b647c
Swagata Das <107251536+swgtds@users.noreply.github.com>
2023-02-17 22:33:51 +0530

    Add files via upload

---
+++ config.h
@@ -0,0 +1,30 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"JetBrains Mono ExtraBold:size=09:antialias=true:autohint=true"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#f8f8f2", "#282a36" },
+	[SchemeSel] = { "#f8f8f2", "#36323d" },
+	[SchemeSelHighlight] = { "#ffc978", "#000000" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeMid] = { "#ffffff", "#222222" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 20;
+static unsigned int min_lineheight = 8;
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+/* Size of the window border */
+static const unsigned int border_width = 4;
